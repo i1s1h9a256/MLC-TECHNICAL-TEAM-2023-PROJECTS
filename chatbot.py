1723f294ecaa54accd1732abd14f41152849bf02
Isha Syed <isha.23bce9003@vitapstudent.ac.in>
2023-11-26 00:42:50 -0600

    added riddle chatbot

---
+++ chatbot.py
@@ -0,0 +1,18 @@
+x = input("answer this question - if a cactus has 32 thorns and half of the thorns have flowers and half of the flowers have 3 fruits each, how many fruits does the cactus have?answer in numbers - ")
+
+if x == "24":
+    y = input('damn really? then i have another question for you - an apple tree has 13 branches each branch has 2 sub branches, out of the two subbranches one has 3 flowers each flower gives an orange how many oranges does this tree have? - ')
+    if y == "0":
+        input("CORRECT!,answer is 0")
+    else:
+        print("an apple tree having oranges? not possible")
+elif x == "0":
+        z = input("dude catus have fruits chk up on google. lets give you another question - a beggar has a brother but the brother doesnt have a brother... how is that possible? -")
+        if z == "idk":
+            print("answer is the beggar is a she,if you got that right then congrats ur not that dumb as i thought")
+        else:
+            print("srsly? THINK!")
+else:
+    print("use ur brain once? u'll atleast get some answer")
+
+    
